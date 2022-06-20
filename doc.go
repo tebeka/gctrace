718e8805beeee5998869d09c9e4b3c89416e9cbb
Miki Tebeka <miki@353solutions.com>
2022-06-20 18:08:30 +0300

    doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+Package gctrace parses Go's GC trace output lines.
+
+See the [runtime] package and [this blog post] for information about the format.
+
+[runtime]: https://pkg.go.dev/runtime#hdr-Environment_Variables
+[blog]: https://www.ardanlabs.com/blog/2019/05/garbage-collection-in-go-part2-gctraces.html
+*/
+package gctrace
--- gctrace.go
+++ gctrace.go
@@ -1,11 +1,3 @@
-/*
-Package gctrace parses Go's GC trace output lines.
-
-See [runtime] & [blog] for information about the format.
-
-[runtime]: https://pkg.go.dev/runtime#hdr-Environment_Variables
-[blog]: https://www.ardanlabs.com/blog/2019/05/garbage-collection-in-go-part2-gctraces.html
-*/
 package gctrace
 
 import (
